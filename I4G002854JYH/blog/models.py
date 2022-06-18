df0e1eb8d226f6bc58cf912c82d3b5a2b895541a
ayoni02 <105247084+ayoni02@users.noreply.github.com>
2022-06-18 14:24:49 +0100

    Added projects

---
+++ I4G002854JYH/blog/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+# Create your models here.
+class Post(models.Model):
+    Title = models.CharField(max_length=200)
+    Text = models.TextField('Any amount of text')
+    Author = models.ForeignKey('Post', on_delete = get_user_model())
+    Created_date = models.DateTimeField('A date-time column')
+    Publish_date = models.DateTimeField('A date-time column')
